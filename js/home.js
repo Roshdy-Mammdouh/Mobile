dce6c4a09709612697b6fdba89c3103386778565
Roshdy Mammdouh <122674292+Roshdy-Mammdouh@users.noreply.github.com>
2023-01-23 17:52:35 +0200

    2

--- js/home.js
+++ js/home.js
@@ -14,16 +14,17 @@ window.onscroll = function() {headerFun()};
 
 function headerFun() {
 
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
       document.getElementById("header").style.padding = "10px";
-      document.getElementById("header").style.backgroundColor = "#1F65DB";
+      document.getElementById("header").classList.add("gradiant-background");
 
       document.getElementById("logo").style.display = "none";
       document.getElementById("logo2").style.display = "block";
     } 
     else {
+        document.getElementById("header").classList.remove("gradiant-background");
         document.getElementById("header").style.padding = "30px 10px";
-        document.getElementById("header").style.backgroundColor = "transparent";
+        
 
       document.getElementById("logo").style.display = "block";
       document.getElementById("logo2").style.display = "none";
